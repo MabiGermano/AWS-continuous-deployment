d46b04eaf965961a8dbcb03bcf8ce01738148d7a
MabiGermano <mabi.germano.e@gmail.com>
2021-08-25 01:19:34 -0300

    creating a bundle with webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+
+module.exports = {
+  target: "node",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  node: "10.19",
+                },
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  resolveLoader: {
+    modules: [__dirname + "/node_modules"],
+  },
+}
