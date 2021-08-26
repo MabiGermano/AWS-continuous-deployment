97902229ce34c8948cc47baec190f408187c2519
MabiGermano <mabi.germano.e@gmail.com>
2021-08-26 00:37:57 -0300

    setting entry point to server.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
   target: "node",
-  entry: "./src/index.js",
+  entry: "./src/server.js",
   output: {
     path: path.join(__dirname, "dist"),
     filename: "main.js",
